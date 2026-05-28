054acfd2a0b6735c84ef271495caf584080eb9f9
BSEMC JEFFREY <bsemc.esparasjeffreyf@gmail.com>
2026-05-28 21:07:48 +0800

    qwerty

--- Assets/Script/MainMenuScripts/SceneChangeTracker.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-// This global tracker stands entirely alone so all scripts can see it instantly
-public static class SceneChangeTracker
-{
-    //heya
-    public static string lastLoadedScene = "";
-
-}
